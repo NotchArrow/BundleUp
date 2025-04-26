f4be80dc05116d45d8399c8218fe85431920b45e
NotchArrow <hokie2031@outlook.com>
2025-04-26 14:43:49 -0400

    Initial Commit

---
+++ src/client/java/com/notcharrow/bundleup/BundleUpClient.java
@@ -0,0 +1,10 @@
+package com.notcharrow.bundleup;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BundleUpClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
